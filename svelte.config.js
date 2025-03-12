0c2d515d739ec15e352843ee4d8c9901f4f39c64
Jonny Smith <jonny.smith@digitaljs.com>
2025-03-12 19:36:53 +0000

    Update SvelteKit configuration to set base path based on NODE_ENV for production deployment

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,7 @@ const config = {
 			precompress: true // Gzip files for faster delivery
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : ''
 		}
 	}
 };
