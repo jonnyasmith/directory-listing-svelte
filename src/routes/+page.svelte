36f3fb8ce22a44fe620920b5af0e7b7fc158f754
Jonny Smith <jonny.smith@digitaljs.com>
2025-03-15 16:32:17 +0000

    Refactor hero and cities sections in +page.svelte for improved layout and readability. Simplify structure by removing unnecessary div wrappers and adjusting spacing, enhancing overall visual consistency and user experience.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,31 +11,27 @@
 </script>
 
 <!-- Hero Section -->
-<section class="hero-section container">
-	<div class="mx-auto max-w-6xl px-4 py-20 text-center">
-		<h2 class="mb-3 text-xl font-semibold">{config.heroTitle}</h2>
-		<h1 class="mb-6 text-5xl font-bold">{config.heroSubtitle}</h1>
-		<p class="mb-4 text-xl">
-			{config.heroDescription1}
-		</p>
-		<p class="mx-auto max-w-3xl text-lg">
-			{config.heroDescription2}
-		</p>
-	</div>
+<section class="hero-section container text-center">
+	<h2 class="mb-3 text-xl font-semibold">{config.heroTitle}</h2>
+	<h1 class="mb-6 text-5xl font-bold">{config.heroSubtitle}</h1>
+	<p class="mb-4 text-xl">
+		{config.heroDescription1}
+	</p>
+	<p class="mx-auto max-w-3xl text-lg">
+		{config.heroDescription2}
+	</p>
 </section>
 
 <!-- Cities Section -->
-<section class="cities-section container">
-	<div class="mx-auto max-w-6xl px-4 py-12">
-		<h2 class="mb-8 text-center text-3xl font-bold">{config.locationSectionTitle}</h2>
+<section class="cities-section container mt-6">
+	<h2 class="mb-8 text-center text-3xl font-bold">{config.locationSectionTitle}</h2>
 
-		<div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-			{#each data.localities as locality}
-				<CompactCard
-					label={`${locality.name}, ${locality.region.shortName}`}
-					href="/{locality.region.slug}/{locality.slug}"
-				/>
-			{/each}
-		</div>
+	<div class="grid grid-cols-2 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+		{#each data.localities as locality}
+			<CompactCard
+				label={`${locality.name}, ${locality.region.shortName}`}
+				href="/{locality.region.slug}/{locality.slug}"
+			/>
+		{/each}
 	</div>
 </section>
